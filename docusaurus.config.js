f895f8e807e3ddb2cb06f0fb3ebf2284b0694bb3
Stanley van den Bos <stanleyvandenbos@gmail.com>
2023-10-22 21:17:43 +0200

    config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,15 +11,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://stanleyvandenbos.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'stanleyvandenbos', // Usually your GitHub org/user name.
+  projectName: 'stanleyvandenbos.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
