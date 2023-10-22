dd36ce2a23221e9cc78924e36a590041c575a2ae
Stanley van den Bos <stanleyvandenbos@gmail.com>
2023-10-22 22:44:40 +0200

    test

--- .docusaurus/docusaurus.config.mjs
+++ .docusaurus/docusaurus.config.mjs
@@ -4,8 +4,8 @@
  * Modify the docusaurus.config.js file at your site's root instead.
  */
 export default {
-  "title": "My Site",
-  "tagline": "Dinosaurs are cool",
+  "title": "Stanley.gg",
+  "tagline": "Securing the Digital World!",
   "favicon": "img/favicon.ico",
   "url": "https://stanleyvandenbos.github.io",
   "baseUrl": "/",
