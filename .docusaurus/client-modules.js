1a45d3777e287d2d098c457b8fd9fb9b617528fc
Stanley van den Bos <stanleyvandenbos@gmail.com>
2023-10-22 21:35:11 +0200

    1

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/home/stanley/Workspace/stanleyvandenbos.github.io/node_modules/infima/dist/css/default/default.css'),
+  require('/home/stanley/Workspace/stanleyvandenbos.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/home/stanley/Workspace/stanleyvandenbos.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/home/stanley/Workspace/stanleyvandenbos.github.io/src/css/custom.css'),
+];
