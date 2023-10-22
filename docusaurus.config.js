f1dad05354aa49638847a62ce459cb7babf0dab4
Stanley van den Bos <stanleyvandenbos@gmail.com>
2023-10-22 21:47:48 +0200

    4

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'stanleyvandenbos', // Usually your GitHub org/user name.
   projectName: 'stanleyvandenbos.github.io', // Usually your repo name.
-  deploymentBranch: 'gh-pages',
+  deploymentBranch: 'main',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
